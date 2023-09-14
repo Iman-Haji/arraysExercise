b2edd95d3de646b34dc603745f87317429304ae0
Iman <iman@Imans-MacBook-Air.local>
2023-09-14 17:27:21 +0300

    css + tasks 1-11

--- array.js
+++ array.js
@@ -6,9 +6,11 @@ const names = ['David', 'Ann', 'Leia', 'Mathew', 'Mark'];
 document.getElementById('task1').textContent = names; //all the names
 
 
+
 // Task 2
 // Print the second item of the array from the task 1.
 console.log(names[1]); // Ann
+
 
 
 // Task 3
@@ -16,9 +18,11 @@ console.log(names[1]); // Ann
 console.log(names.length); // 5
 
 
+
 // Task 4
 // Print the last item of the array
 console.log(names[names.length -1]); // Mark
+
 
 
 // Task 5
@@ -27,11 +31,10 @@ names.push('Peter');
 console.log(names); // adds the name Peter
 
 
+
 // Task 6
 // Print the array with spaces, not commas
 document.querySelector('#task6').textContent = names.join('  '); // gives spaces
-
-//or 
 
 //names.join(' ');
 console.log(names.join(' ')); // gives spaces 
@@ -48,26 +51,34 @@ console.log(names);
 
 // Task 8
 // Sort the array in the alphabetical order and print it
-
-
-
-
-//names.sort('  ');
-//document.querySelector('#task8').textContent = names;
-
+names.sort();
+console.log(names.sort());
 
 
 // Task 9
 // Create an array called numbers and fill it with values 1, 7, 45, 32, 27, 86.
 // Sort the numbers from largest to smallest. Print them.
+let numbers = [1, 7, 45, 32, 27, 86];
+numbers.sort(function(a,b){return a - b});
+console.log(numbers);
 
 
 
 // Task 10
 // Randomize the numbers array.
 
+// using 0.5 is most equal. Ifyou add something else other than 0,5, then it won’t be equal to 0 to 1.
+numbers.sort((a,b) => 0.5 - Math.random());
+console.log(numbers.sort());
+
 
 
 // Task 11
 // Find the largest and smallest value from the numbers array using sort() function.
 // Print those values.
+//numbers.sort()
+numbers.sort(function(a,b) {return a-b});
+let smallestValue = numbers[0]; 
+let largestValue= numbers[numbers.length -1];
+console.log(smallestValue);
+console.log(largestValue);
